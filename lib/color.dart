5af356912447f80e61bb48a92e4563baf8fa3533
FardanAthilla <fardanathilla.gmail.com>
2024-04-23 09:04:34 +0700

    Nambah Warna

---
+++ lib/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Warna {
+  static Color main = Color(0xFF0D99FF);
+  static Color background = Color(0xFFFAFAFA);
+  static Color card = Color(0xffF1F1F1);
+  static Color teks = Color(0xff000000);
+  static Color teksactive = Color(0xffFFFFFF);
+  static Color white = Color(0xffD5EFF2);
+  static Color danger = Color(0xffFF0000);
+  static Color hintteks = Color.fromRGBO(0, 0, 0, 0.5);
+}
