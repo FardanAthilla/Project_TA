e8615ff37f2cdd19519a32291502e86463ff7ad0
howhigare <aylasyafaa23@gmail.com>
2024-04-24 09:10:17 +0700

    feat:[AS] perubahan

--- lib/Page/home_page/homepage.dart
+++ lib/Page/home_page/homepage.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: Center(
           child: Center(
-        child: Text('Ini adalah'),
+        child: Text('Ini adalah halaman 1'),
       ),
         ));
   }
