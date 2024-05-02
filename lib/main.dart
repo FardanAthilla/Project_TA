be2cab8b453d2eebe00020109aea1b3bedad219b
FardanAthilla <fardanathilla.gmail.com>
2024-05-02 15:37:20 +0700

    Feat: [FAH] Bikin Tabbar dan List untuk daftar mesin

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MainApp extends StatelessWidget {
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       home: SafeArea(
-        child: LoginPage(),
+        child: Navigation(),
       ),
     );
   }
