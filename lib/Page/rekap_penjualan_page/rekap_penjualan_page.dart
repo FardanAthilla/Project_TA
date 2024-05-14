06b83774050c80ae49b64486aef200002abe8eea
FardanAthilla <fardanathilla.gmail.com>
2024-05-14 13:36:22 +0700

    feat: [FAH] pisahin controller

--- lib/Page/rekap_penjualan_page/rekap_penjualan_page.dart
+++ lib/Page/rekap_penjualan_page/rekap_penjualan_page.dart
@@ -1,46 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:project_ta/Page/rekap_penjualan_page/Model/controller.dart';
 import 'package:project_ta/color.dart';
-
-class SalesReportController extends GetxController {
-  RxList salesData = [].obs;
-  RxList filteredSalesData = [].obs;
-
-  Future<void> fetchSalesReport() async {
-    final response = await http
-        .get(Uri.parse('https://secure-sawfly-certainly.ngrok-free.app/sales'));
-
-    if (response.statusCode == 200) {
-      salesData.value = json.decode(response.body)['Data'];
-      filteredSalesData.value = salesData;
-    } else {
-      throw Exception('Failed to load sales report');
-    }
-  }
-
-  void filterSalesData(String keyword) {
-    if (keyword.isEmpty) {
-      filteredSalesData.value = salesData;
-    } else {
-      filteredSalesData.value = salesData.where((data) {
-        return data['item_name']
-                .toLowerCase()
-                .contains(keyword.toLowerCase()) ||
-            data['branch'].toLowerCase().contains(keyword.toLowerCase());
-      }).toList();
-    }
-  }
-
-  @override
-  void onInit() {
-    super.onInit();
-    fetchSalesReport();
-  }
-}
 
 class RekapLaporanPenjualan extends StatelessWidget {
   final SalesReportController controller = Get.put(SalesReportController());
