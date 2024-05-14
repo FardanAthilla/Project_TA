ab2e19dcb5cbc0d80b9da04f2dc12b0ed83ce69e
howhigare <aylasyafaa23@gmail.com>
2024-05-14 08:01:41 +0700

    feat: [AS] make laporan service page

---
+++ lib/Page/laporan_service_page/laporan_service_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LaporanServicePage extends StatelessWidget {
+  const LaporanServicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
