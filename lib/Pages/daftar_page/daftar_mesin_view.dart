31603ea609a9c2192a6697bbaa49ea1df89a5917
howhigare <aylasyafaa23@gmail.com>
2024-05-20 12:15:37 +0700

    feat: [AS]change to sliverappbar

--- lib/Pages/daftar_page/daftar_mesin_view.dart
+++ lib/Pages/daftar_page/daftar_mesin_view.dart
@@ -16,181 +16,178 @@ class DaftarMesin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        surfaceTintColor: Warna.background,
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            surfaceTintColor: Warna.background,
+            title: SvgPicture.asset(
               'Assets/logo2.svg',
               width: 30,
               height: 30,
-            )
-          ],
-        ),
-      ),
-      body: Obx(() {
-        if (storeController.isLoading.value) {
-          return _buildShimmer();
-        } else {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  children: [
-                    const Expanded(
-                      child: SizedBox(
-                        height: 48,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Cari sekarang...',
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.all(Radius.circular(9.0)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.blue),
-                              borderRadius: BorderRadius.all(Radius.circular(9.0)),
-                            ),
-                            prefixIcon: Icon(Icons.search),
-                            contentPadding: EdgeInsets.symmetric(vertical: 14.0),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: SizedBox(
+                      height: 48,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Cari sekarang...',
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                            borderRadius: BorderRadius.all(Radius.circular(9.0)),
                           ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                            borderRadius: BorderRadius.all(Radius.circular(9.0)),
+                          ),
+                          prefixIcon: Icon(Icons.search),
+                          contentPadding: EdgeInsets.symmetric(vertical: 14.0),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 12),
-                    GestureDetector(
-                      onTap: () {
-                        _showFilterModal(context);
-                      },
-                      child: Container(
-                        height: 48,
-                        width: 48,
-                        decoration: BoxDecoration(
-                          color: Warna.main,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Icon(Icons.filter_list, color: Warna.background),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Kategori Mesin',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
                     ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: RefreshIndicator(
-                  onRefresh: _refreshItems,
-                  child: ListView.builder(
-                    itemCount: storeController.filteredItems.length,
-                    itemBuilder: (context, index) {
-                      final item = storeController.filteredItems[index];
-                      final category = storeController.categories.firstWhere(
-                        (cat) =>
-                            cat.categoryMachineId == item.categoryMachineId,
-                        orElse: () => CategoryMachine(
-                          categoryMachineId: 0,
-                          categoryMachineName: 'Unknown Category',
-                        ),
-                      );
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 12.0,
-                              right: 12.0,
-                              top: 8.0,
-                              bottom: 8.0,
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(12.0),
-                                  child: Image.asset(
-                                    'Assets/iconlistmesin3.png',
-                                    width: 80,
-                                    height: 80,
-                                    fit: BoxFit.contain,
-                                  ),
-                                ),
-                                const SizedBox(width: 16),
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        item.storeItemsName,
-                                        style: TextStyle(
-                                          fontSize: 15,
-                                          color: Warna.hitam,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        category.categoryMachineName,
-                                        style: TextStyle(
-                                          color: Warna.card,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w100,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 8),
-                                      Text(
-                                        'Rp.${item.price}',
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          color: Warna.teks,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    '${item.quantity} Stok',
-                                    style: TextStyle(
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.normal,
-                                      color: Warna.hitam,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const Divider(
-                            indent: 15,
-                            endIndent: 15,
-                          ),
-                        ],
-                      );
+                  const SizedBox(width: 12),
+                  GestureDetector(
+                    onTap: () {
+                      _showFilterModal(context);
                     },
+                    child: Container(
+                      height: 48,
+                      width: 48,
+                      decoration: BoxDecoration(
+                        color: Warna.main,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(Icons.filter_list, color: Warna.background),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Kategori Mesin',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
               ),
-            ],
-          );
-        }
-      }),
+            ),
+          ),
+          Obx(() {
+            if (storeController.isLoading.value) {
+              return SliverFillRemaining(
+                child: _buildShimmer(),
+              );
+            } else {
+              return SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final item = storeController.filteredItems[index];
+                    final category = storeController.categories.firstWhere(
+                      (cat) => cat.categoryMachineId == item.categoryMachineId,
+                      orElse: () => CategoryMachine(
+                        categoryMachineId: 0,
+                        categoryMachineName: 'Unknown Category',
+                      ),
+                    );
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12.0,
+                            right: 12.0,
+                            top: 8.0,
+                            bottom: 8.0,
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: Image.asset(
+                                  'Assets/iconlistmesin3.png',
+                                  width: 80,
+                                  height: 80,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item.storeItemsName,
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Warna.hitam,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      category.categoryMachineName,
+                                      style: TextStyle(
+                                        color: Warna.card,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w100,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Rp.${item.price}',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Warna.teks,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  '${item.quantity} Stok',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.normal,
+                                    color: Warna.hitam,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          indent: 15,
+                          endIndent: 15,
+                        ),
+                      ],
+                    );
+                  },
+                  childCount: storeController.filteredItems.length,
+                ),
+              );
+            }
+          }),
+        ],
+      ),
     );
   }
 
@@ -341,7 +338,8 @@ class DaftarMesin extends StatelessWidget {
                                 height: 15,
                                 color: Colors.white,
                               ),
-                              const SizedBox(height: 8),
+                              const SizedBox
+                              (height: 8),
                               Container(
                                 height: 12,
                                 color: Colors.white,
