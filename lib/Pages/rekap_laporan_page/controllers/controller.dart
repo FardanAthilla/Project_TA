c8ea3ef7bac8fc1397b3bddbf7d457758ff6e2a2
howhigare <aylasyafaa23@gmail.com>
2024-05-22 07:17:44 +0700

    feat: [AS] fix double pages, and make controller rekap

--- lib/Pages/rekap_laporan_page/controllers/controller.dart
+++ lib/Pages/rekap_laporan_page/controllers/controller.dart
@@ -8,26 +8,13 @@ class SalesReportController extends GetxController {
 
   Future<void> fetchSalesReport() async {
     final response = await http
-        .get(Uri.parse('https://secure-sawfly-certainly.ngrok-free.app/sales'));
+        .get(Uri.parse('https://rdo-app-o955y.ondigitalocean.app/sales'));
 
     if (response.statusCode == 200) {
       salesData.value = json.decode(response.body)['Data'];
-      filteredSalesData.value = salesData;
+      // filteredSalesData.value = salesData;
     } else {
       throw Exception('Failed to load sales report');
-    }
-  }
-
-  void filterSalesData(String keyword) {
-    if (keyword.isEmpty) {
-      filteredSalesData.value = salesData;
-    } else {
-      filteredSalesData.value = salesData.where((data) {
-        return data['item_name']
-                .toLowerCase()
-                .contains(keyword.toLowerCase()) ||
-            data['branch'].toLowerCase().contains(keyword.toLowerCase());
-      }).toList();
     }
   }
 
