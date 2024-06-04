04c980a254120ccc19bdcdce610d2e6f1ae529d3
FardanAthilla <fardanathilla.gmail.com>
2024-06-04 18:33:35 +0700

    feat: [FAH] nambah lupa password

--- lib/Pages/navigation/routes/route.dart
+++ lib/Pages/navigation/routes/route.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:project_ta/Pages/login_page/login_page_view.dart';
+import 'package:project_ta/Pages/login_page/pages/login_page_view.dart';
 import 'package:project_ta/Pages/navigation/navbar_view.dart';
 import 'package:project_ta/Pages/splash_screen/splash_screen.view.dart';
 
