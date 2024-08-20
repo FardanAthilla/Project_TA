5533fd8a00de84a4da3a7235795f98ca881c93b1
FardanAthilla <fardanathilla.gmail.com>
2024-08-20 21:00:54 +0700

    feat: [FAH] betulin dikit

--- lib/Pages/navigation/navbar_view.dart
+++ lib/Pages/navigation/navbar_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -12,6 +14,7 @@ import 'package:project_ta/Pages/rekap_laporan_page/controllers/fetchsales.dart'
 import 'package:project_ta/Pages/rekap_laporan_page/controllers/fetchservice.dart';
 import 'package:project_ta/Pages/rekap_laporan_page/rekap_page.dart';
 import 'package:project_ta/color.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Navbar extends StatelessWidget {
   final NavbarController controller = Get.put(NavbarController());
@@ -87,58 +90,79 @@ class Navbar extends StatelessWidget {
     ];
   }
 
+  DateTime? lastBackPressTime;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Obx(() => IndexedStack(
-              index: controller.selectedIndex.value,
-              children: _buildScreens(),
-            )),
-        bottomNavigationBar: Container(
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 255, 255, 255),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10.0),
-              topRight: Radius.circular(10.0),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                spreadRadius: 0,
-                blurRadius: 10,
-                offset: Offset(0, -1),
-              ),
-            ],
-          ),
-          child: Obx(() => BottomNavigationBar(
-                items: _navbarItems(),
-                currentIndex: controller.selectedIndex.value,
-                selectedItemColor: Colors.blue,
-                unselectedItemColor: Colors.black,
-                onTap: (index) {
-                  controller.onItemTapped(index);
-                  if (index == 0) {
-                    storeController.searchItems(storeController.searchController.text);
-                    storeController.fetchStoreItems();
-                    sparepartController.searchItems(sparepartController.searchController.text);
-                    sparepartController.fetchStoreItems();
-                  }
-                  if (index == 1) {
-                    storeController.ItemSelect(storeController.searchControllerReport.text);
-                    profileController.fetchUserData();
-                    sparepartController.SparePartSelect(sparepartController.searchControllerReport.text);
-                    sparepartController.SparePartSelectService(sparepartController.searchControllerService.text);
-                  }
-                  if (index == 2) {
-                    Get.find<SalesController>().fetchSalesReports();
-                    Get.find<FetchServiceController>().fetchServiceReports();
-                  }
-                },
-                type: BottomNavigationBarType.fixed,
-                backgroundColor: Colors.transparent,
-                elevation: 0,
+    return WillPopScope(
+      onWillPop: () async {
+        DateTime now = DateTime.now();
+        if (lastBackPressTime == null ||
+            now.difference(lastBackPressTime!) > Duration(seconds: 2)) {
+          lastBackPressTime = now;
+          Fluttertoast.showToast(
+            msg: "Klik kembali lagi untuk keluar",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            backgroundColor: Colors.black54,
+            textColor: Colors.white,
+            fontSize: 16.0,
+          );
+          return Future.value(false);
+        }
+        return Future.value(true);
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: Obx(() => IndexedStack(
+                index: controller.selectedIndex.value,
+                children: _buildScreens(),
               )),
+          bottomNavigationBar: Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 0,
+                  blurRadius: 10,
+                  offset: Offset(0, -1),
+                ),
+              ],
+            ),
+            child: Obx(() => BottomNavigationBar(
+                  items: _navbarItems(),
+                  currentIndex: controller.selectedIndex.value,
+                  selectedItemColor: Colors.blue,
+                  unselectedItemColor: Colors.black,
+                  onTap: (index) {
+                    controller.onItemTapped(index);
+                    if (index == 0) {
+                      storeController.searchItems(storeController.searchController.text);
+                      storeController.fetchStoreItems();
+                      sparepartController.searchItems(sparepartController.searchController.text);
+                      sparepartController.fetchStoreItems();
+                    }
+                    if (index == 1) {
+                      storeController.ItemSelect(storeController.searchControllerReport.text);
+                      profileController.fetchUserData();
+                      sparepartController.SparePartSelect(sparepartController.searchControllerReport.text);
+                      sparepartController.SparePartSelectService(sparepartController.searchControllerService.text);
+                    }
+                    if (index == 2) {
+                      Get.find<SalesController>().fetchSalesReports();
+                      Get.find<FetchServiceController>().fetchServiceReports();
+                    }
+                  },
+                  type: BottomNavigationBarType.fixed,
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                )),
+          ),
         ),
       ),
     );
